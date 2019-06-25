75794b2f1063b79bb906a3f423c9851831e340e9
Samantha (sam) Silverstein <selinons@fb.com>
2019-06-25 14:11:07 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by selinons on 6/25/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
